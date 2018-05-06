9be99357bc1145344062bc0eb82fdb94b43c09c5
Cody Goodman <codygman.consulting@gmail.com>
2018-05-06 18:07:24 -0500

    add selenium and chromeDriver to cabal extra-libraries and regen nix files
    cabal2nix . > default.nix
    cabal2nix --shell . > shell.nix

--- default.nix
+++ default.nix
@@ -1,11 +1,15 @@
-{ mkDerivation, base, stdenv }:
+{ mkDerivation, base, chromeDriver, selenium-server-standalone
+, stdenv, webdriver
+}:
 mkDerivation {
   pname = "webdriver-nixos-haskell-example";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base ];
-  executableSystemDepends = [ selenium-server-standalone chromeDriver ];
+  executableHaskellDepends = [ base webdriver ];
+  executableSystemDepends = [
+    chromeDriver selenium-server-standalone
+  ];
   license = stdenv.lib.licenses.bsd3;
 }
--- shell.nix
+++ shell.nix
@@ -4,14 +4,19 @@ let
 
   inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, base, stdenv }:
+  f = { mkDerivation, base, chromeDriver
+      , selenium-server-standalone, stdenv, webdriver
+      }:
       mkDerivation {
         pname = "webdriver-nixos-haskell-example";
         version = "0.1.0.0";
         src = ./.;
         isLibrary = false;
         isExecutable = true;
-        executableHaskellDepends = [ base ];
+        executableHaskellDepends = [ base webdriver ];
+        executableSystemDepends = [
+          chromeDriver selenium-server-standalone
+        ];
         license = stdenv.lib.licenses.bsd3;
       };
 
