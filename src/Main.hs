c212edbf76ee7f03381b27957161017325c16f75
Cody Goodman <codygman.consulting@gmail.com>
2018-05-06 18:37:25 -0500

    add chromedriver example

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Test.WebDriver
+
+chromeConfig = useBrowser chrome defaultConfig
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = runSession chromeConfig $ do                      -- starts a WebDriver session with the given firefox config, then
+                                                          -- runs the supplied commands
+
+  openPage "http://google.com"                            -- tells the browser to open the URL http://google.com
+
+  searchInput <- findElem ( ByCSS "input[type='text']" )  -- asks the browser to find an element on the page with the given
+                                                          -- CSS selector then stores the resulting element in the variable
+                                                          -- named`searchInput`
+
+  sendKeys "Hello, World!" searchInput                    -- type into the element, as though a user had issued the
+                                                          -- keystrokes `Hello, World!`
+
+  submit searchInput                                      -- submit the input form (technically not required with Google
+                                                          -- but included for example purposes)
+
+  closeSession                                            -- finally, close the WebDriver session and its associated
+                                                          -- browser process
